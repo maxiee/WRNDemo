1df6b788a87e47f0aa6750f7ee7b36bc8fc50d03
WangRui <maxieewong@gmail.com>
2018-05-29 22:30:24 +0800

    首页

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import {
 } from 'react-native';
 import * as rssParser from 'react-native-rss-parser';
 import HTML from 'react-native-render-html';
+import { Styles } from './src/ui/style';
 
 const instructions = Platform.select({
 	ios: 'Press Cmd+R to reload,\n' +
@@ -28,6 +29,20 @@ interface State {
 	data: any
 }
 
+interface GridButtonProps {
+	icon: string,
+	title: string,
+	background: string
+}
+const GridButton = (props: GridButtonProps) => {
+	return (
+		<View style={[styles.gridButton, {backgroundColor: props.background}]}>
+			<Text style={Styles.h1}>{props.title.substr(1, 2)}</Text>
+			<Text>{props.title}</Text>
+		</View>
+	)
+}
+
 export default class App extends Component<Props, State> {
 	constructor(props: Props) {
 		super(props);
@@ -36,19 +51,19 @@ export default class App extends Component<Props, State> {
 		};
 	}
 	
-	componentDidMount() {
-		fetch("https://rsshub.app/weibo/user/3306934123")
-		.then(response => response.text())
-		.then(responseData => rssParser.parse(responseData))
-		.then(rss => {
-			console.log(rss.title)
-			console.log(rss.items.length)
-			rss.items.forEach((v: any) => {
-				console.log(v.title)
-			})
-			this.setState({data: rss})
-		})
-	}
+	// componentDidMount() {
+	// 	fetch("https://rsshub.app/weibo/user/3306934123")
+	// 	.then(response => response.text())
+	// 	.then(responseData => rssParser.parse(responseData))
+	// 	.then(rss => {
+	// 		console.log(rss.title)
+	// 		console.log(rss.items.length)
+	// 		rss.items.forEach((v: any) => {
+	// 			console.log(v.title)
+	// 		})
+	// 		this.setState({data: rss})
+	// 	})
+	// }
 	
 	render() {
 		let d = <Text>加载中</Text>;
@@ -64,7 +79,14 @@ export default class App extends Component<Props, State> {
 			})
 		}
 		return (
-			<ScrollView>{d}</ScrollView>
+			<View style={styles.container}>
+				<Text style={Styles.h1}>想干啥？</Text>
+				<View style={styles.grid}>
+					<GridButton title="刷微博" icon="微博" background="gold"/>
+					<GridButton title="刷微博" icon="微博" background="gold"/>
+					<GridButton title="刷微博" icon="微博" background="gold"/>
+				</View>
+			</View>
 		);
 	}
 }
@@ -72,18 +94,21 @@ export default class App extends Component<Props, State> {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
+		flexDirection: 'column',
+	},
+	grid: {
+		flexDirection: 'row',
+		justifyContent: "space-evenly",
+	},
+	gridButton: {
+		borderRadius: 6,
+		borderColor: 'grey',
+		paddingTop: 14,
+		paddingBottom: 14,
+		paddingLeft: 28,
+		paddingRight: 28,
+		flexDirection: 'column',
 		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	},
-	welcome: {
-		fontSize: 20,
-		textAlign: 'center',
-		margin: 10,
-	},
-	instructions: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5,
-	},
+		borderWidth: 1
+	}
 });
---
+++ src/ui/style.tsx
@@ -0,0 +1,10 @@
+import {
+    StyleSheet
+} from 'react-native'
+
+export const Styles = StyleSheet.create({
+    h1: {
+        fontSize: 30,
+        fontWeight: 'bold'
+    }
+})
